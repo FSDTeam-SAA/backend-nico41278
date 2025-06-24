8b0d80a1fc57df751c21f84a4d49b14efdd69ec8
zihadulislam2 <zihadul708@gmail.com>
2025-06-24 11:36:32 +0600

    feat: add CORS middleware for handling cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -9,20 +9,25 @@ import newsletterRoutes from './routes/newsLetter.route'
 import agentRoutes from './routes/agent.routes'
 import userListingRoutes from './routes/userListing.routes'
 import userMessageRoutes from './routes/userMessage.routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(
+  cors({
+    origin: '*',
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  })
+)
 app.use(express.json())
 
 app.use('/api/v1', userRoutes)
 app.use('/api/v1', paymentRoutes)
 app.use('/api/v1', subscriptionRoute)
-app.use('/api/v1', newsletterRoutes )
+app.use('/api/v1', newsletterRoutes)
 app.use('/api/v1', agentRoutes)
 app.use('/api/v1', userListingRoutes)
 app.use('/api/v1', userMessageRoutes)
-
-
 
 app.use(notFound as never)
 app.use(globalErrorHandler)
